03f252f29c464bee992e1d7f4a703de9c22d5869
Joel Hooks <joelhooks@gmail.com>
2010-03-06 11:09:15 -0600

    further removed items from start branch to make it even more starty

--- src/sql/DeleteContact.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM main.contacts
-WHERE contactId = :contactId
--- src/sql/create/PopulateContacts.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO main.contacts (contactId, firstName, lastName, address, city, state, zip, phone, email)
-SELECT 1, 'Joel', 'Hooks', '2222 A St.', 'Fort Worth', 'TX', '76112', '817-555-1432', 'joelhooks@gmail.com' UNION
-SELECT 2, 'Paul', 'Robertson', '1401 B St.', 'Dublin', 'CA', '94568', '925-555-1110', 'paul@probertson.com'
